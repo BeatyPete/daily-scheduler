04c4637ad240df4dcf9943ed3c7b24a10d2a2ffc
BeatyPete <akira941@gmail.com>
2020-11-03 04:51:10 -0600

    added function to compare time slots and add color

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+var tasks = [];
+
 //get current time
 var getTime = function() {
     var now = moment();
     displayDate(now);
+    compareTime(now);
 };
 
 //current day display
 var displayDate = function(currentTime) {
     var date = currentTime.format("dddd, MMMM Do")
     $("#currentDay").html(date)
-    console.log(date);
 };
+
+//determine color of timeblocks
+var compareTime = function(currentTime) {
+    var timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    for(var i = 0; i < timeSlots.length; i++) {
+        var time = moment().hour(timeSlots[i]).minutes(0);
+        var slotEl = "#" + timeSlots[i];
+        if (currentTime.isBefore(time)) {
+            $(slotEl).addClass("future")
+        }
+        else if (currentTime.isSame(time, "hour")) {
+            $(slotEl).addClass("present")
+        }
+        else if (currentTime.isAfter(time,)) {
+            $(slotEl).addClass("past")
+        }
+    };
+}
 
 getTime();
